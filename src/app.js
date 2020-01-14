8a6b431ae7696aa8726c685be6c6c218d0b47ec5
Evan Smith <smith.m.evan@gmail.com>
2020-01-14 22:50:18 +0000

    pulling correct data into select

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+      euros: null,
+      currencies: {
+        rates: {} 
+      },
+      selectedCurrency: null
+    },
+    mounted: function() {
+     this.fetchCurrencies()
+    },
+    methods: {
+      fetchCurrencies: function (){
+        fetch('https://api.exchangeratesapi.io/latest')
+        .then(response => response.json())
+        .then(currencies => this.currencies = currencies)
+      }
+    }
+  });
+});
